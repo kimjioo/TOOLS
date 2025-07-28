5fe62adca4c08e89c7ea3974d976b477c6496df3
kimjioo <82633446+kimjioo@users.noreply.github.com>
2025-07-28 16:17:44 +0800

    Create BBR.sh

---
+++ BBR.sh
@@ -0,0 +1,12 @@
+# 创建并写入 BBR 配置
+sudo tee /etc/sysctl.d/99-bbr.conf <<EOF
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+EOF
+
+# 应用配置
+sudo sysctl -p
+
+# 打印当前拥塞控制算法以进行验证
+echo "TCP BBR 已配置完成。当前拥塞控制算法为："
+sysctl net.ipv4.tcp_congestion_control
